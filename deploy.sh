3009c322a9256d5facebcdfd38821f92de7e983a
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2021-03-09 12:48:03 +0100

    add changes from upstream

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -x
 mkdir _build
 
 # Modify as need to deploy on different networks
-TZC="../Tezos/tezos/tezos-client --mode mockup --base-dir _build/mockup"
+TZC="../Tezos/tezos/tezos-client --mode mockup --protocol PtEdo2ZkT9oKpimTah6x2embF25oss54njMuPzkJTEi5RqfdZFA --base-dir _build/mockup"
 
 rm -rf _build/mockup
 $TZC create mockup
@@ -21,7 +21,7 @@ CTEZ_ADDRESS=`$TZC show known contract ctez`
 # Build and deploy the fa12 for ctez
 ligo compile-contract fa12.mligo main > _build/fa12.tz
 ligo compile-storage fa12.mligo main "$(sed s/ADMIN_ADDRESS/$CTEZ_ADDRESS/ < fa12_ctez_initial_storage.mligo)" > _build/fa12_ctez_storage.tz
-$TZC originate contract fa12_ctez transferring 0 from $deployment_key running 'file:_build/fa12.tz' --init "$(<_build/fa12_ctez_storage.tz)" --burn-cap 10 
+$TZC originate contract fa12_ctez transferring 0 from $deployment_key running 'file:_build/fa12.tz' --init "$(<_build/fa12_ctez_storage.tz)" --burn-cap 10
 FA12_CTEZ_ADDRESS=`$TZC show known contract fa12_ctez`
 
 # Build and deploy cfmm
@@ -29,12 +29,12 @@ ligo compile-contract cfmm_tez_ctez.mligo main > _build/cfmm.tz
 sed s/FA12_CTEZ/${FA12_CTEZ_ADDRESS}/ < cfmm_initial_storage.mligo | sed s/CTEZ_ADDRESS/${CTEZ_ADDRESS}/ > _build/cfmm_storage.mligo
 
 ligo compile-storage cfmm_tez_ctez.mligo main "$(<_build/cfmm_storage.mligo)" > _build/cfmm_storage.tz
-$TZC originate contract cfmm transferring 0.000001 from $deployment_key running 'file:_build/cfmm.tz' --init "$(<_build/cfmm_storage.tz)" --burn-cap 10 
+$TZC originate contract cfmm transferring 0.000001 from $deployment_key running 'file:_build/cfmm.tz' --init "$(<_build/cfmm_storage.tz)" --burn-cap 10
 CFMM_ADDRESS=`$TZC show known contract cfmm`
 
 # Build and deploy the fa12 for the cfmm lqt, specifying the cfmm as admin
 ligo compile-storage fa12.mligo main "$(sed s/ADMIN_ADDRESS/$CFMM_ADDRESS/ < fa12_ctez_initial_storage.mligo)" > _build/fa12_lqt_storage.tz
-$TZC originate contract fa12_lqt transferring 0 from $deployment_key running 'file:_build/fa12.tz' --init "$(<_build/fa12_lqt_storage.tz)" --burn-cap 10 
+$TZC originate contract fa12_lqt transferring 0 from $deployment_key running 'file:_build/fa12.tz' --init "$(<_build/fa12_lqt_storage.tz)" --burn-cap 10
 FA12_LQT_ADDRESS=`$TZC show known contract fa12_lqt`
 
 # Set the lqt fa12 address in the cfmm
