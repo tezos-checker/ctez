c0810f6a997723503ab93b72d65cbee47243b879
--email <murbard@users.noreply.github.com>
2021-03-16 13:09:52 +0000

    fix computation of liquidity and drift

--- frontend/app/src/api/contracts.ts
+++ frontend/app/src/api/contracts.ts
@@ -7,10 +7,10 @@ export const getBaseStats = async (): Promise<BaseStats> => {
   const cfmmStorage = await getCfmmStorage();
   const currentTarget = cTezStorage.target.toNumber() / 2 ** 48;
   const currentPrice = cfmmStorage.cashPool.toNumber() / cfmmStorage.tokenPool.toNumber();
-  const premium = currentPrice === currentTarget ? 0 : currentPrice / currentTarget - 1;
+  const premium = currentPrice === currentTarget ? 0 : currentPrice / currentTarget - 1.0;
   const drift = cTezStorage.drift.toNumber();
-  const currentAnnualDrift = (1 + drift / 2 ** 48) ** (365.25 * 24 * 3600 - 1);
-  const totalLiquidity = cfmmStorage.cashPool.toNumber() * 2;
+  const currentAnnualDrift = (1.0 + drift / 2 ** 48) ** (365.25 * 24 * 3600) - 1.0;
+  const totalLiquidity = cfmmStorage.cashPool.toNumber() * 2 / 1e6;
   return {
     currentTarget,
     currentPrice,
