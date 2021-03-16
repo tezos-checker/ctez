72a8dfa9df768fab802b1137a29e263c8a6d8399
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2021-03-16 20:15:13 +0100

    add icon to liquidity

--- frontend/app/src/components/OvenStats/OvenStats.tsx
+++ frontend/app/src/components/OvenStats/OvenStats.tsx
@@ -5,6 +5,38 @@ import { useQuery } from 'react-query';
 import { getBaseStats } from '../../api/contracts';
 import { BaseStats } from '../../interfaces';
 import { StatsCard } from '../StatsCard/StatsCard';
+import TezosIcon from '../TezosIcon';
+import { Typography } from '../Typography';
+
+interface StatCardExtraProps {
+  value: string;
+  isPercentage?: boolean;
+  icon?: React.FC<any>;
+}
+
+const StatCardExtra: React.FC<StatCardExtraProps> = ({ value, isPercentage, icon: Icon }) => {
+  const val = isPercentage ? `${value}%` : value;
+  return (
+    <>
+      {Icon ? (
+        <Grid container spacing={3}>
+          <Grid item>
+            <Icon />
+          </Grid>
+          <Grid item>
+            <Typography size="subtitle1" component="p" textAlign="center" marginTop="0.4rem">
+              {val}
+            </Typography>
+          </Grid>
+        </Grid>
+      ) : (
+        <Typography size="subtitle1" component="p" textAlign="center">
+          {val}
+        </Typography>
+      )}
+    </>
+  );
+};
 
 export const OvenStats: React.FC = () => {
   const { t } = useTranslation(['common']);
@@ -18,11 +50,13 @@ export const OvenStats: React.FC = () => {
         {stats &&
           Object.keys(stats).map((item, index) => (
             <Grid item key={`${index}`} md={3} xs="auto" lg={2}>
-              <StatsCard
-                label={t(item)}
-                value={stats[item]}
-                isPercentage={item === 'premium' || item === 'currentAnnualDrift'}
-              />
+              <StatsCard label={t(item)}>
+                <StatCardExtra
+                  value={stats[item]}
+                  isPercentage={item === 'premium' || item === 'currentAnnualDrift'}
+                  icon={item === 'totalLiquidity' ? TezosIcon : undefined}
+                />
+              </StatsCard>
             </Grid>
           ))}
       </Grid>
--- frontend/app/src/components/StatsCard/StatsCard.tsx
+++ frontend/app/src/components/StatsCard/StatsCard.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import styled from '@emotion/styled';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+import { Card, CardContent } from '@material-ui/core';
 import { Typography } from '../Typography/Typography';
 
 const StyledCard = styled(Card)`
@@ -11,21 +10,16 @@ const StyledCard = styled(Card)`
 
 interface StatsCardProps {
   label: string;
-  value: string | number;
-  isPercentage?: boolean;
 }
 
-export const StatsCard: React.FC<StatsCardProps> = ({ label, value, isPercentage }) => {
-  const val = isPercentage ? `${value}%` : value;
+export const StatsCard: React.FC<StatsCardProps> = ({ label, children }) => {
   return (
     <StyledCard>
       <CardContent>
         <Typography size="h6" component="div" textAlign="center">
           {label}
         </Typography>
-        <Typography size="subtitle1" component="p" textAlign="center">
-          {val}
-        </Typography>
+        {children}
       </CardContent>
     </StyledCard>
   );
