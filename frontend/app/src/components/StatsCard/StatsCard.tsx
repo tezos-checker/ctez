a39b78498e733d55f48ae938122dae7954813564
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2021-03-17 09:49:12 +0100

    fix card size

--- frontend/app/src/components/StatsCard/StatsCard.tsx
+++ frontend/app/src/components/StatsCard/StatsCard.tsx
@@ -6,6 +6,8 @@ import { Typography } from '../Typography/Typography';
 const StyledCard = styled(Card)`
   min-width: 10rem;
   max-width: 15rem;
+  min-height: 7.2rem;
+  max-height: 7.2rem;
 `;
 
 interface StatsCardProps {
@@ -16,7 +18,7 @@ export const StatsCard: React.FC<StatsCardProps> = ({ label, children }) => {
   return (
     <StyledCard>
       <CardContent>
-        <Typography size="h6" component="div" textAlign="center">
+        <Typography size="h6" component="div" textAlign="center" whiteSpace="nowrap">
           {label}
         </Typography>
         {children}
