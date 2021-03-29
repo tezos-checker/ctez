f14182c0e13b2fc0273d187ae6a4bbe5b50697eb
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2021-03-29 14:54:26 +0200

    fix: update calculations

--- frontend/app/src/pages/BuySell/Conversion.tsx
+++ frontend/app/src/pages/BuySell/Conversion.tsx
@@ -60,13 +60,14 @@ const ConvertComponent: React.FC<ConversionParams> = ({ t, formType }) => {
     },
   );
 
-  const calcMinBuyValue = (slippage: number, cashSold: number) => {
+  const calcMinBuyValue = (slippage: number, amount: number) => {
     if (cfmmStorage) {
       const { tokenPool, cashPool } = cfmmStorage;
+      const cashSold = amount * 10 ** 7;
       const [aPool, bPool] =
         formType === 'tezToCtez' ? [tokenPool, cashPool] : [cashPool, tokenPool];
       const tokWithoutSlippage =
-        (cashSold * 997 * aPool.toNumber()) / (bPool.toNumber() * 1000 + cashSold * 997);
+        (cashSold * 997 * aPool.toNumber()) / (bPool.toNumber() * 1000 + cashSold * 997) / 10 ** 7;
       const tok = tokWithoutSlippage * (1 - slippage * 0.01);
       setWithoutSlippage(Number(tokWithoutSlippage.toFixed(6)));
       setMinBuyValue(Number(tok.toFixed(6)));
