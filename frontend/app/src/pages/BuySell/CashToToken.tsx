c21144ba8d54b86ee871cf05b82de3a4e4b581f1
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2021-03-29 11:51:09 +0200

    feat: update form cash to token

--- frontend/app/src/pages/BuySell/CashToToken.tsx
+++ frontend/app/src/pages/BuySell/CashToToken.tsx
@@ -1,44 +1,83 @@
+import { useState } from 'react';
 import * as Yup from 'yup';
+import { addMinutes } from 'date-fns';
 import { withTranslation, WithTranslation } from 'react-i18next';
 import styled from '@emotion/styled';
+import { useQuery } from 'react-query';
+import { AxiosError } from 'axios';
 import { Field, Form, Formik } from 'formik';
-import { Button, Grid, Paper, InputAdornment } from '@material-ui/core';
+import { Button, Grid, Paper, InputAdornment, Typography } from '@material-ui/core';
 import { useToasts } from 'react-toast-notifications';
 import { useHistory } from 'react-router-dom';
 import Page from '../../components/Page';
 import FormikTextField from '../../components/TextField';
 import { useWallet } from '../../wallet/hooks';
-import { CashToTokenParams } from '../../interfaces';
-import { cashToToken, cfmmError } from '../../contracts/cfmm';
-import { FormikDateTimePicker } from '../../components/DateTimePicker';
+import { CfmmStorage } from '../../interfaces';
+import { cashToToken, cfmmError, getCfmmStorage } from '../../contracts/cfmm';
 import { TezosIcon } from '../../components/TezosIcon';
-import { CTezIcon } from '../../components/CTezIcon/CTezIcon';
+import { logger } from '../../utils/logger';
 
 const PaperStyled = styled(Paper)`
   padding: 2em;
 `;
 
+interface CashToTokenForm {
+  to: string;
+  slippage: number;
+  deadline: number;
+  amount: number;
+}
+
 const CashToTokenComponent: React.FC<WithTranslation> = ({ t }) => {
   const [{ pkh: userAddress }] = useWallet();
   const { addToast } = useToasts();
   const history = useHistory();
-  const initialValues: CashToTokenParams = {
+  const [minTokens, setMinTokens] = useState(0);
+  const { data: cfmmStorage } = useQuery<CfmmStorage, AxiosError, CfmmStorage>(
+    ['cfmmStorage'],
+    async () => {
+      return getCfmmStorage();
+    },
+    {
+      refetchInterval: 30000,
+      staleTime: 3000,
+    },
+  );
+
+  const calcMinTokens = (slippage: number, cashSold: number): number => {
+    if (cfmmStorage) {
+      const { tokenPool, cashPool } = cfmmStorage;
+      const tok =
+        ((cashSold * 997 * tokenPool.toNumber()) / (cashPool.toNumber() * 1000 + cashSold * 997)) *
+        (1 - slippage);
+      return Number(tok.toFixed(6));
+    }
+    return -1;
+  };
+
+  const initialValues: CashToTokenForm = {
     to: userAddress ?? '',
-    minTokensBought: 0,
-    deadline: new Date(new Date().getTime() + 5 * 60000),
+    slippage: 0,
+    deadline: 20,
     amount: 0,
   };
 
   const validationSchema = Yup.object().shape({
     to: Yup.string().required(t('required')),
-    minTokensBought: Yup.number().required(t('required')),
-    deadline: Yup.date().required(t('required')),
+    slippage: Yup.number().optional(),
+    deadline: Yup.number().min(0).required(t('required')),
     amount: Yup.number().required(t('required')),
   });
 
-  const handleFormSubmit = async (data: CashToTokenParams) => {
+  const handleFormSubmit = async (formData: CashToTokenForm) => {
     try {
-      const result = await cashToToken(data);
+      const deadline = addMinutes(new Date(), formData.deadline);
+      const result = await cashToToken({
+        amount: formData.amount,
+        deadline,
+        minTokensBought: minTokens,
+        to: formData.to,
+      });
       if (result) {
         addToast('Transaction Submitted', {
           appearance: 'success',
@@ -47,6 +86,7 @@ const CashToTokenComponent: React.FC<WithTranslation> = ({ t }) => {
         });
       }
     } catch (error) {
+      logger.warn(error);
       const errorText = cfmmError[error.data[1].with.int as number] || 'Transaction Failed';
       addToast(errorText, {
         appearance: 'error',
@@ -62,7 +102,7 @@ const CashToTokenComponent: React.FC<WithTranslation> = ({ t }) => {
         validationSchema={validationSchema}
         onSubmit={handleFormSubmit}
       >
-        {({ isSubmitting, isValid }) => (
+        {({ isSubmitting, isValid, values }) => (
           <PaperStyled>
             <Form>
               <Grid
@@ -85,24 +125,6 @@ const CashToTokenComponent: React.FC<WithTranslation> = ({ t }) => {
                 <Grid item>
                   <Field
                     component={FormikTextField}
-                    name="minTokensBought"
-                    id="minTokensBought"
-                    label={t('minTokensBought')}
-                    className="minTokensBought"
-                    type="number"
-                    fullWidth
-                    InputProps={{
-                      startAdornment: (
-                        <InputAdornment position="start">
-                          <CTezIcon height={30} width={30} />
-                        </InputAdornment>
-                      ),
-                    }}
-                  />
-                </Grid>
-                <Grid item>
-                  <Field
-                    component={FormikTextField}
                     name="amount"
                     id="amount"
                     label={t('xtzToPay')}
@@ -116,18 +138,62 @@ const CashToTokenComponent: React.FC<WithTranslation> = ({ t }) => {
                         </InputAdornment>
                       ),
                     }}
+                    handleChange={(
+                      e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
+                    ) => {
+                      const { slippage } = values;
+                      const min = calcMinTokens(slippage, Number(e.target.value));
+                      setMinTokens(min);
+                    }}
                   />
                 </Grid>
                 <Grid item>
                   <Field
-                    component={FormikDateTimePicker}
+                    component={FormikTextField}
+                    name="slippage"
+                    id="slippage"
+                    label={t('slippage')}
+                    type="number"
+                    fullWidth
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <Typography>%</Typography>
+                        </InputAdornment>
+                      ),
+                    }}
+                    handleChange={(
+                      e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
+                    ) => {
+                      const { amount } = values;
+                      const min = calcMinTokens(Number(e.target.value), amount);
+                      setMinTokens(min);
+                    }}
+                  />
+                </Grid>
+                {minTokens > -1 && (
+                  <Grid item>
+                    <Typography>
+                      {t('minCtezBought')}: {minTokens}
+                    </Typography>
+                  </Grid>
+                )}
+                <Grid item>
+                  <Field
+                    component={FormikTextField}
                     name="deadline"
                     id="deadline"
-                    label={t('deadline')}
-                    inputFormat="dd/MM/yyyy HH:mm"
+                    label={t('transactionTimeout')}
                     className="deadline"
                     type="number"
-                    disablePast
+                    fullWidth
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <Typography>{t('minutes')}</Typography>
+                        </InputAdornment>
+                      ),
+                    }}
                   />
                 </Grid>
                 <Grid item>
