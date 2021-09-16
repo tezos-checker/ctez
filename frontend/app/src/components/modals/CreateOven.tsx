539bbdb612c6de821e98de6c9fff9d93bd8c2c16
Vijay Pushkin <mail@vijaypushkin.me>
2021-09-16 14:01:57 +0530

    Create Oven UI created

--- frontend/app/src/components/modals/CreateOven.tsx
+++ frontend/app/src/components/modals/CreateOven.tsx
@@ -1,5 +1,9 @@
 import {
   Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -7,29 +11,211 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Select,
+  useRadioGroup,
+  useToast,
 } from '@chakra-ui/react';
+import { validateAddress } from '@taquito/utils';
+import { useEffect, useState } from 'react';
+import { array, number, object, string } from 'yup';
+import { useTranslation } from 'react-i18next';
+import { useFormik } from 'formik';
+import { useDelegates } from '../../api/queries';
+import { Depositor } from '../../interfaces';
+import { create, cTezError } from '../../contracts/ctez';
+import { useWallet } from '../../wallet/hooks';
+import { logger } from '../../utils/logger';
+import RadioCard from '../radio/RadioCard';
 
 interface ICreateOvenProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
-const CreateOven: React.FC<ICreateOvenProps> = ({ isOpen, onClose }) => {
-  return (
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Modal Title</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>Hello</ModalBody>
+interface ICreateVaultForm {
+  delegate: string;
+  amount: number;
+  depositType: 'Whitelist' | 'Everyone';
+  depositors: string;
+  depositorOp: Depositor;
+}
 
-        <ModalFooter>
-          <Button colorScheme="blue" mr={3} onClick={onClose}>
-            Close
-          </Button>
-          <Button variant="ghost">Secondary Action</Button>
-        </ModalFooter>
-      </ModalContent>
+const CreateOven: React.FC<ICreateOvenProps> = ({ isOpen, onClose }) => {
+  const [{ pkh: userAddress }] = useWallet();
+  const { data: delegates } = useDelegates(userAddress);
+  const [delegate, setDelegate] = useState('');
+  const toast = useToast();
+  const { t } = useTranslation(['common']);
+  const options = ['Whitelist', 'Everyone'];
+
+  const validationSchema = object().shape({
+    delegate: string()
+      .test({
+        test: (value) => validateAddress(value) === 3,
+      })
+      .required(t('required')),
+    amount: number().optional(),
+    depositors: array()
+      .test({
+        test: (value) => {
+          return (
+            value?.reduce((acc, item: any) => {
+              return acc && validateAddress(item?.value ?? item) === 3;
+            }, true) ?? false
+          );
+        },
+      })
+      .required(t('required')),
+  });
+
+  const opSelectionList = [
+    {
+      label: t(Depositor.whitelist),
+      value: Depositor.whitelist,
+    },
+    {
+      label: t(Depositor.any),
+      value: Depositor.any,
+    },
+  ];
+
+  const defaultDepositorList: string[] = [
+    userAddress,
+    delegate !== '' ? delegate : undefined,
+  ].filter((x): x is string => x !== undefined);
+
+  const [initialValues, setInitialValues] = useState<ICreateVaultForm>({
+    delegate,
+    amount: 0,
+    depositors: (userAddress ? defaultDepositorList : []).join(', '),
+    depositType: 'Whitelist',
+    depositorOp: Depositor.whitelist,
+  });
+
+  useEffect(() => {
+    const newState: ICreateVaultForm = {
+      ...initialValues,
+      delegate,
+      depositors: (userAddress ? defaultDepositorList : []).join(', '),
+    };
+    setInitialValues(newState);
+  }, [userAddress, delegate]);
+
+  const handleFormSubmit = async (data: ICreateVaultForm) => {
+    if (userAddress) {
+      try {
+        const depositors =
+          data.depositors.split(', ').length > 0 && data.depositorOp === Depositor.whitelist
+            ? data.depositors
+                .split(', ')
+                .map((item: string) => item)
+                .filter((o) => o !== userAddress)
+            : undefined;
+        const result = await create(
+          userAddress,
+          data.delegate,
+          data.depositorOp,
+          depositors,
+          data.amount,
+        );
+        if (result) {
+          toast({
+            description: t('txSubmitted'),
+            status: 'success',
+          });
+        }
+      } catch (error) {
+        logger.error(error);
+        const errorText = cTezError[error.data[1].with.int as number] || t('txFailed');
+        toast({
+          description: errorText,
+          status: 'error',
+        });
+      }
+    }
+  };
+
+  const { values, handleChange, handleSubmit, ...formik } = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: handleFormSubmit,
+  });
+
+  const { getRootProps, getRadioProps } = useRadioGroup({
+    name: 'depositType',
+    defaultValue: 'Whitelist',
+    onChange: (value) => formik.setFieldValue('depositType', value),
+  });
+  const group = getRootProps();
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <ModalOverlay />
+      <form onSubmit={handleSubmit}>
+        <ModalContent>
+          <ModalHeader>Create an Oven</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl w="100%" mb={2}>
+              <FormLabel fontSize="xs">Delegate</FormLabel>
+
+              <Select
+                placeholder={t('delegatePlaceholder')}
+                value={values.delegate}
+                onChange={(ev) => {
+                  formik.setFieldValue('delegate', ev.target.value);
+                }}
+              >
+                {delegates?.map((x) => (
+                  <option key={x.address} value={x.address}>
+                    {x.address}
+                  </option>
+                ))}
+              </Select>
+            </FormControl>
+            <FormControl w="100%" mb={2}>
+              <FormLabel fontSize="xs">Initial Deposit</FormLabel>
+              <Input
+                type="number"
+                name="amount"
+                id="amount"
+                value={values.amount}
+                onChange={handleChange}
+              />
+            </FormControl>
+
+            <FormControl w="100%" mb={2}>
+              <FormLabel fontSize="xs">Who can Deposit?</FormLabel>
+              <Flex {...group} w="100%" justifyContent="space-between">
+                {options.map((value) => {
+                  const radio = getRadioProps({ value });
+                  return (
+                    <RadioCard key={value} {...radio}>
+                      {value}
+                    </RadioCard>
+                  );
+                })}
+              </Flex>
+            </FormControl>
+
+            <FormControl w="100%" mb={2}>
+              <FormLabel fontSize="xs">Authorised Deposters</FormLabel>
+              <Input
+                name="depositors"
+                id="depositors"
+                value={values.depositors}
+                onChange={handleChange}
+              />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" w="100%" type="submit">
+              Create Oven
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </form>
     </Modal>
   );
 };
