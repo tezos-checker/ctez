3804a511c4d9cbdbdf7f9c9e140b5a4dd83e91a7
Vijay Pushkin <mail@vijaypushkin.me>
2021-10-15 13:35:39 +0530

    Removed unwanted type

--- frontend/app/src/router/routes.tsx
+++ frontend/app/src/router/routes.tsx
@@ -11,32 +11,27 @@ export const routes: ComponentRoute[] = [
   {
     path: '/myovens/:ovenId',
     Component: <OvenIdPage />,
-    pageTitle: 'Oven Details',
   },
   {
     path: '/myovens',
     Component: <OvensPage />,
     exact: true,
-    pageTitle: 'My Ovens',
   },
 
   {
     path: '/ovens',
     Component: <OvensPage />,
     exact: true,
-    pageTitle: 'All Ovens',
   },
   {
     path: '/trade',
     Component: <TradePage />,
     exact: true,
-    pageTitle: 'Trade',
   },
   {
     path: '/faq',
     Component: <FaqPage />,
     exact: true,
-    pageTitle: 'FAQ',
   },
   // ? Default path must always be at the end
   {
