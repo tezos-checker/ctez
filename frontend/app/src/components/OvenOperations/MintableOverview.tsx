a359cc66e97f1c14fc7df6d9ec5e4fd038253a6b
Ishwaryaa <ishwaryaa@tezsure.com>
2021-10-25 15:18:10 +0530

    added remaining mintable tooltip content

--- frontend/app/src/components/OvenOperations/MintableOverview.tsx
+++ frontend/app/src/components/OvenOperations/MintableOverview.tsx
@@ -58,7 +58,7 @@ const MintableOverview: React.FC<{ oven: AllOvenDatum | null }> = ({ oven }) => 
         <Flex mr={-2} ml={-2} p={2} borderRadius={14} backgroundColor={cardbg}>
           <Icon fontSize="2xl" color="#B0B7C3" as={MdInfo} m={1} />
           <Text color="gray.500" fontSize="xs" ml={2}>
-            {data.find((item) => item.topic === 'mintable')?.content}
+            {data.find((item) => item.topic === 'remaining mintable')?.content}
           </Text>
         </Flex>
       </div>
