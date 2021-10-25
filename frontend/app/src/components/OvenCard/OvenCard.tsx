8a7376c0986132837434fa9cf4bb489c2c1e7461
Vijay Pushkin <mail@vijaypushkin.me>
2021-10-25 10:25:13 +0530

    Ovens Card mobile view added

--- frontend/app/src/components/OvenCard/OvenCard.tsx
+++ frontend/app/src/components/OvenCard/OvenCard.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  CSSObject,
   Flex,
   Grid,
   Tag,
@@ -7,6 +8,7 @@ import {
   TagLeftIcon,
   Text,
   useColorModeValue,
+  useMediaQuery,
 } from '@chakra-ui/react';
 import React, { useMemo } from 'react';
 import { Link } from 'react-router-dom';
@@ -15,38 +17,93 @@ import { AllOvenDatum } from '../../interfaces';
 import ProgressPill from './ProgressPill';
 import { useOvenStats } from '../../hooks/utilHooks';
 import CopyAddress from '../CopyAddress/CopyAddress';
-import { useCtezBaseStats, useUserBalance } from '../../api/queries';
+import { useCtezBaseStats } from '../../api/queries';
 import { isMonthFromLiquidation } from '../../api/contracts';
-import { useWallet } from '../../wallet/hooks';
 import SkeletonLayout from '../skeleton';
+import { trimAddress } from '../../utils/addressUtils';
 
 interface IOvenCardProps {
   type: 'AllOvens' | 'MyOvens';
   oven: AllOvenDatum;
 }
 
-const truncateText = (text: string | null) => {
-  if (text == null) {
-    return '';
-  }
-
-  const len = text.length;
-  return `${text.substr(0, 5)}...${text.substr(len - 5)}`;
-};
-
 const OvenCard: React.FC<IOvenCardProps> = (props) => {
-  const [{ pkh: userAddress }] = useWallet();
   const background = useColorModeValue('white', 'cardbgdark');
   const textcolor = useColorModeValue('text2', 'white');
   const imported = useColorModeValue('blue', 'white');
   const { stats } = useOvenStats(props.oven);
   const { data } = useCtezBaseStats();
+  const [largerScreen] = useMediaQuery(['(min-width: 800px)']);
   const result = isMonthFromLiquidation(
     Number(stats?.outStandingCtez),
     Number(data?.currentTarget),
     Number(stats?.ovenBalance ?? 0),
     Number(data?.currentAnnualDrift),
   );
+
+  // ? Used for changing layout between Mobile and Desktop view
+  const cssSxValue: { outerFlex: CSSObject; innerGrid: CSSObject } = useMemo(() => {
+    if (largerScreen) {
+      return {
+        outerFlex: {
+          my: 6,
+          py: 4,
+          px: 10,
+        },
+        innerGrid: {
+          gridTemplateColumns: 'repeat(5, 3fr) 4fr',
+        },
+      };
+    }
+
+    const getBorderStyles = (position: ('Bottom' | 'Right')[]) => {
+      return {
+        borderColor: 'gray.200',
+        borderStyle: 'solid',
+        ...position.reduce((acc, cur) => ({ ...acc, [`border${cur}Width`]: `1px` }), {}),
+      };
+    };
+
+    return {
+      outerFlex: {
+        my: 6,
+        py: 2,
+        px: 6,
+      },
+      innerGrid: {
+        gridTemplateColumns: `repeat(6, 1fr)`,
+        gridTemplateRows: `repeat(3, 1fr)`,
+
+        '& > div': {
+          p: 4,
+        },
+
+        '& > #oven-card-item-1': {
+          gridArea: `1 / 1 / 2 / 4`,
+          ...getBorderStyles(['Bottom', 'Right']),
+        },
+        '& > #oven-card-item-2': {
+          gridArea: `1 / 4 / 2 / 7`,
+          ...getBorderStyles(['Bottom']),
+        },
+        '& > #oven-card-item-3': {
+          gridArea: `2 / 1 / 3 / 3`,
+          ...getBorderStyles(['Bottom', 'Right']),
+        },
+        '& > #oven-card-item-4': {
+          gridArea: `2 / 3 / 3 / 5`,
+          ...getBorderStyles(['Bottom', 'Right']),
+        },
+        '& > #oven-card-item-5': {
+          gridArea: `2 / 5 / 3 / 7`,
+          ...getBorderStyles(['Bottom']),
+        },
+        '& > #oven-card-item-6': {
+          gridArea: `3 / 1 / 4 / 7`,
+        },
+      },
+    };
+  }, [largerScreen]);
 
   const renderedItems = useMemo(() => {
     const { address, owner, id } = (() => {
@@ -62,7 +119,7 @@ const OvenCard: React.FC<IOvenCardProps> = (props) => {
         return {
           label: 'Owner',
           value: owner,
-          displayValue: truncateText(owner),
+          displayValue: trimAddress(owner),
         };
       }
 
@@ -75,11 +132,11 @@ const OvenCard: React.FC<IOvenCardProps> = (props) => {
 
     const items = [
       props.type === 'MyOvens' && firstItem(),
-      { label: 'Oven address', value: address, displayValue: truncateText(address) },
+      { label: 'Oven address', value: address, displayValue: trimAddress(address) },
       props.type === 'AllOvens' && {
         label: 'Owner',
         value: owner,
-        displayValue: truncateText(owner),
+        displayValue: trimAddress(owner),
       },
       {
         label: 'Oven Balance',
@@ -96,12 +153,14 @@ const OvenCard: React.FC<IOvenCardProps> = (props) => {
         value: `${stats?.maxMintableCtez} ctez`,
         displayValue: `${stats?.maxMintableCtez} ctez`,
       },
-    ].filter((x): x is { label: string; value: string; displayValue: string } => !!x);
+    ]
+      .filter((x): x is { label: string; value: string; displayValue: string } => !!x)
+      .map((x, i) => ({ ...x, id: `oven-card-item-${i + 1}` }));
 
     return (
       <>
         {items.map((item) => (
-          <Box key={item.label}>
+          <Box key={item.id} id={item.id}>
             {item.label === 'Oven address' || item.label === 'Owner' ? (
               <Text as="span" color={textcolor} fontWeight="600">
                 <CopyAddress address={item.value}>{item.displayValue}</CopyAddress>
@@ -116,7 +175,7 @@ const OvenCard: React.FC<IOvenCardProps> = (props) => {
             </Text>
           </Box>
         ))}
-        <Box>
+        <Box id="oven-card-item-6">
           <ProgressPill
             value={Number(stats?.collateralUtilization ?? 0)}
             type={props.type}
@@ -131,23 +190,20 @@ const OvenCard: React.FC<IOvenCardProps> = (props) => {
     );
   }, [
     props.type,
-    props.oven.value.address,
-    props.oven.key.owner,
-    props.oven.key.id,
-    props.oven.isImported,
+    props.oven,
     stats?.ovenBalance,
     stats?.outStandingCtez,
     stats?.maxMintableCtez,
     stats?.collateralUtilization,
+    result,
     textcolor,
   ]);
 
   const content = (
     <Flex
       direction="column"
-      my={6}
-      py={4}
-      px={10}
+      sx={cssSxValue.outerFlex}
+      minW="340px"
       borderRadius={16}
       backgroundColor={background}
       _hover={
@@ -169,7 +225,7 @@ const OvenCard: React.FC<IOvenCardProps> = (props) => {
           <TagLabel>Imported</TagLabel>
         </Tag>
       )}
-      <Grid gridTemplateColumns="repeat(5, 3fr) 4fr">{renderedItems}</Grid>
+      <Grid sx={cssSxValue.innerGrid}>{renderedItems}</Grid>
     </Flex>
   );
 
