0f91a0f3d8a2765c6377c2ee5a82dce01ac72c33
Ishwaryaa <ishwaryaa@tezsure.com>
2021-10-26 18:07:14 +0530

    removed unused variable and added ctez in lowercaps

--- frontend/app/src/components/modals/Burn.tsx
+++ frontend/app/src/components/modals/Burn.tsx
@@ -42,7 +42,6 @@ const Burn: React.FC<IBurnProps> = ({ isOpen, onClose, oven }) => {
   const { t } = useTranslation(['common']);
   const toast = useToast();
   const text2 = useColorModeValue('text2', 'darkheading');
-  const text4 = useColorModeValue('text4', 'darkheading');
   const text1 = useColorModeValue('text1', 'darkheading');
   const inputbg = useColorModeValue('darkheading', 'textboxbg');
   const cardbg = useColorModeValue('bg3', 'darkblue');
@@ -139,7 +138,7 @@ const Burn: React.FC<IBurnProps> = ({ isOpen, onClose, oven }) => {
       <form onSubmit={handleSubmit}>
         <ModalContent>
           <ModalHeader color={text1} fontWeight="500">
-            Burn cTez
+            Burn ctez
           </ModalHeader>
           <ModalCloseButton />
           <ModalBody>
